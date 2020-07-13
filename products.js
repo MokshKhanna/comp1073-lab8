2b8fd8d91ab6b99757194894b2bc49d457879c76
MokshKhanna <65981799+MokshKhanna@users.noreply.github.com>
2020-07-13 08:13:02 -0400

    Lab 8

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 // created a XMLHttpRequest 
 let request = new XMLHttpRequest();
 // request for json data
-request.open('GET','./products.json');
+request.open('GET','https://mokshkhanna.github.io/comp1073-lab8/products.json');
 // set responsetype
 request.responseType = 'json';
 // send the request
@@ -18,20 +18,25 @@ request.onload = function(){
 function weirdDeals(json)
 {
 		// getting topDeals list
-	    let topDeals = json.weirdDeals;
+	    let topDeals = json.topDeals;
+		// getting list
 		let list = document.getElementById('weirdDeals');
+		// iterating over the deas
 		for (let i = 0; i < topDeals.length; i++) {
+			// creating elements for each details
         let li = document.createElement('li');
         let h3 = document.createElement('h3');
         let img = document.createElement('img');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
+		// adding  values in attributes
         img.setAttribute('src', './images/' + topDeals[i].image);
         img.setAttribute('height', '100px');
         img.setAttribute('width', '100px');
         h3.textContent = topDeals[i].name;
         p1.textContent = 'Price ' + topDeals[i].price;
         p2.textContent = 'Features are  ' + topDeals[i].features;
+		// creating final element for list
         li.appendChild(img);
         li.appendChild(h3);
         li.appendChild(p1);
